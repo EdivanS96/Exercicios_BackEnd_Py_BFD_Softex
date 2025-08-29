6a399426ab2fa4cce596bfb33e96c85b3b48cc86
Edivan Silva <164213518+EdivanS96@users.noreply.github.com>
2025-08-29 14:52:51 -0300

    Questão 1

---
+++ Exe1/Questão1.py
@@ -0,0 +1,15 @@
+# Q1 - Contar vogais e consoantes:
+frase = input("Digite uma frase: ").lower()
+
+vogais = "aeiou"
+cont_vogais = 0
+cont_consoantes = 0
+
+for c in frase:
+    if c.isalpha():
+        if c in vogais:
+            cont_vogais += 1
+        else:
+            cont_consoantes += 1
+
+print(f"Vogais: {cont_vogais} | Consoantes: {cont_consoantes}")
